a349b72ced5905b9f7e2068af64bd6fb4efa32f0
Rob | Labs <roblabs@users.noreply.github.com>
2016-10-27 20:15:24 -0700

    /codepen.io/metricsgraphics-js-scientific-data * Function on('click') listener

--- metricsgraphics-js-scientific-data/js/index.js
+++ metricsgraphics-js-scientific-data/js/index.js
@@ -1,3 +1,6 @@
+console.clear();
+
+
 var multiline = "\
 function fake_days(input) {\n  \
 \
@@ -14,15 +17,17 @@ function fake_days(input) {\n  \
 \
 var input = JSON.parse(document.querySelector(\".data textarea\").value);\n  \
 var many_many_many_days = fake_days(input);\n  \
+var mouseOverIndex = 0;\n  \
 var timerId = 0;  \n  \
 var z = MG.data_graphic({\n  \
-  onclick: function(d, i){\n  \
-      console.log(i + \" click\");\n  \
+  click: function(d, i){\n  \
+      console.log(i + \" was clicked\");\n  \
   },\n  \
   mouseover: function(d, i) {\n  \
     timerId = setTimeout(function() {\n  \
-        console.log(i + \" 2 seconds have elapsed\");\n  \
-    }, 750);\n  \
+        mouseOverIndex = i;\n  \
+        console.log(\"mouseover on index = \" + i);\n  \
+    }, 50);\n  \
   },\n  \
   mouseout: function(d, i) {\n  \
     clearTimeout(timerId);\n  \
@@ -35,36 +40,31 @@ var z = MG.data_graphic({\n  \
   height: 250,\n  \
   target: \".result\",\n  \
 });\n  \
+\n  \
+ d3.select('.result').on('click', \n  \
+ function() {\n  \
+              console.log(\"clicked on index \" + mouseOverIndex);\n  \
+              var data = JSON.parse(document.querySelector(\".data textarea\").value);\n  \
+              var month = d3.select(\"#month\");\n  \
+              month.html(data[mouseOverIndex].date);\n  \
+        });\n  \
 ";
 
 
+var editor = ace.edit("editor");
+editor.getSession().setMode("ace/mode/javascript");
+editor.setValue(multiline);
+editor.gotoLine(1);
+editor.setHighlightActiveLine(false);
 
-var minify = "function fake_days(a){for(var b=[],c=0;c<a.length;c++){var d=a[c].date,e=new Date(d);v=a[c].threshold_none,b.push({date:MG.clone(e),value:v})}return b}var input=JSON.parse(document.querySelector(\".data textarea\").value),many_many_many_days=fake_days(input),timerId=0,z=MG.data_graphic({onclick:function(a,b){console.log(b+\" click\")},mouseover:function(a,b){timerId=setTimeout(function(){console.log(b+\" 2 seconds have elapsed\")},750)},mouseout:function(a,b){clearTimeout(timerId)},title:\"Chlorophyll Concentration\",description:\"Description\",data:many_many_many_days,markers:[{date:2008,label:'\"Thor\" released'}],width:400,height:250,target:\".result\"});";
 
-var default_call = '//modify away!\n'
-            + 'MG.data_graphic({\n'
-            + '  title: "UFO Sightings",\n'
-            + '  description: "Yearly UFO sightings from 1945 to 2010.",\n'
-            + '  data: JSON.parse(document.querySelector(\'.data textarea\').value),\n'
-            + '  markers: [{\'year\': 1964, \'label\': \'"The Creeping Terror" released\'}],\n'
-            + '  width: 400,\n'
-            + '  height: 250,\n'
-            + '  target: ".result",\n'
-            + '  x_accessor: "year",\n'
-            + '  y_accessor: "sightings",\n'
-            + '});';
+d3.json('https://gist.githubusercontent.com/roblabs/436e5460a640170fc77033d362fb0bd0/raw/d45c86c83679ce923ee52905b70fee202a21e577/8-day.json', function(data) {
 
-        var editor = ace.edit("editor");
-        editor.getSession().setMode("ace/mode/javascript");
-        editor.setValue(multiline);
-        editor.gotoLine(1);
-        editor.setHighlightActiveLine(false);
-
-        d3.json('https://gist.githubusercontent.com/roblabs/436e5460a640170fc77033d362fb0bd0/raw/d45c86c83679ce923ee52905b70fee202a21e577/8-day.json', function(data) {
             document.querySelector('.data textarea').value = JSON.stringify(data, null, 2);
             eval(editor.getValue());
-        })
+        });
 
-        d3.select('.update').on('click', function() {
+// Create a click handler for the "run" button
+d3.select('.update').on('click', function() {
             eval(editor.getValue());
         })
